d55cb8b2313bc9e74b2d315ca93354f15b525d6f
tecnogram888 <civilizationarchitect@gmail.com>
2022-11-20 23:33:53 -0800

    RootApp match only on exact path

--- src/components/router.js
+++ src/components/router.js
@@ -11,7 +11,7 @@ class RouterApp extends React.Component {
     render() {
         return <Router>
             <div>
-                <Route path="/" component={RootApp} />
+                <Route exact path="/" component={RootApp} />
                 <Route path="/app1" component={App1} />
                 <Route path="/app2" component={App2} />
                 <Route path="/login" component={Login} />
