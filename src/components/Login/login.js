c268f648b0ce61d6dbc4cec03695680c216c0ada
tecnogram888 <civilizationarchitect@gmail.com>
2023-02-19 11:42:56 -0800

    add state to Login Class Component

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '', 
+            password: '',
+        }
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+
+    handleUsernameChange(e) {
+        this.setState({username: e.target.value})
+    }
+
+    handlePasswordChange(e) {
+        this.setState({password: e.target.value})
+    }
+
     render() {
+        let {username, password} = this.state
+
         return <div>
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" /><br /><br />
+            <input type="text" id="username" name="username" value={username} onInput={this.handleUsernameChange} /><br /><br />
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" /><br /><br />
+            <input type="password" id="password" name="password" value={password} onInput={this.handlePasswordChange} /><br /><br />
             <input type="submit" value="Submit" />
         </div>
     }
