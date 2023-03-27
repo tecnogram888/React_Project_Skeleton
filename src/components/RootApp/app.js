6b721204c88bde560da8623773ff4185fae9ab3f
tecnogram888 <civilizationarchitect@gmail.com>
2023-03-27 13:33:49 -0700

    Play with State

--- src/components/RootApp/app.js
+++ src/components/RootApp/app.js
@@ -2,14 +2,30 @@ import React from 'react'
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import css from './../../css/style.css'
-
 class RootApp extends React.Component {
+
+    state = {
+        a: 2
+    }
+
+    componentDidMount() {
+        try {
+            fetch('http://localhost:8000').then(data => data.json())
+            .then(data => {
+                console.log(data)
+                this.setState({a: 3})
+            }).catch((err) => console.log(err));
+        } catch (e) {
+            console.log('err, ', e)
+        }
+    }
+
     render() {
+        console.log('the state', this.state)
         return <div>
             <h1 className="asdf">Welcome</h1>
-            <p><Link to="/loginClass/">Login Class Compnoent</Link></p>
-            <p><Link to="/loginFunctional/">Login Functional Compnoent</Link></p>
+            <p><Link to="/loginClass/">Login Class Component</Link></p>
+            <p><Link to="/loginFunctional/">Login Functional Component</Link></p>
             <p><Link to="/app1/">App1</Link></p>
             <p><Link to="/app2/">App2</Link></p>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import _ from "lodash";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./store";
-import RouterApp from "./components/router";
+import _ from 'lodash';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
+import RouterApp from './components/router';
 
-const container = document.getElementById("app");
+const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Provider store={store}>
