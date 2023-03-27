dc8498342e80e0905b3526dd3dc1673dde8122d2
tecnogram888 <civilizationarchitect@gmail.com>
2023-03-27 13:35:58 -0700

    Prettier Router and App Files

--- src/components/RootApp/app.js
+++ src/components/RootApp/app.js
@@ -1,37 +1,48 @@
-import React from 'react'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class RootApp extends React.Component {
+  state = {
+    a: 2,
+  };
 
-    state = {
-        a: 2
+  componentDidMount() {
+    try {
+      fetch('http://localhost:8000')
+        .then((data) => data.json())
+        .then((data) => {
+          console.log(data);
+          this.setState({ a: 3 });
+        })
+        .catch((err) => console.log(err));
+    } catch (e) {
+      console.log('err, ', e);
     }
+  }
 
-    componentDidMount() {
-        try {
-            fetch('http://localhost:8000').then(data => data.json())
-            .then(data => {
-                console.log(data)
-                this.setState({a: 3})
-            }).catch((err) => console.log(err));
-        } catch (e) {
-            console.log('err, ', e)
-        }
-    }
-
-    render() {
-        console.log('the state', this.state)
-        return <div>
-            <h1 className="asdf">Welcome</h1>
-            <p><Link to="/loginClass/">Login Class Component</Link></p>
-            <p><Link to="/loginFunctional/">Login Functional Component</Link></p>
-            <p><Link to="/app1/">App1</Link></p>
-            <p><Link to="/app2/">App2</Link></p>
-        </div>
-    }
+  render() {
+    console.log('the state', this.state);
+    return (
+      <div>
+        <h1 className="asdf">Welcome</h1>
+        <p>
+          <Link to="/loginClass/">Login Class Component</Link>
+        </p>
+        <p>
+          <Link to="/loginFunctional/">Login Functional Component</Link>
+        </p>
+        <p>
+          <Link to="/app1/">App1</Link>
+        </p>
+        <p>
+          <Link to="/app2/">App2</Link>
+        </p>
+      </div>
+    );
+  }
 }
 
-const wrappedApp = connect()(RootApp)
+const wrappedApp = connect()(RootApp);
 
-export default wrappedApp
+export default wrappedApp;
--- src/components/router.js
+++ src/components/router.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import RootApp from './RootApp/app'
-import App1 from './App1/app'
-import App2 from './App2/app'
-import LoginClassComponent from './Login/loginClassComponent'
-import LoginFunctionalComponent from './Login/loginFunctionalComponent'
+import RootApp from './RootApp/app';
+import App1 from './App1/app';
+import App2 from './App2/app';
+import LoginClassComponent from './Login/loginClassComponent';
+import LoginFunctionalComponent from './Login/loginFunctionalComponent';
 
 const customHistory = createBrowserHistory();
 
 class RouterApp extends React.Component {
-    render() {
-        return <Router history={customHistory}>
-            <div>
-                <Route exact path="/" component={RootApp} />
-                <Route path="/app1" component={App1} />
-                <Route path="/app2" component={App2} />
-                <Route path="/loginClass" component={LoginClassComponent} />
-                <Route path="/loginFunctional" component={LoginFunctionalComponent} />
-            </div>
-        </Router>
-    }
+  render() {
+    return (
+      <Router history={customHistory}>
+        <div>
+          <Route exact path="/" component={RootApp} />
+          <Route path="/app1" component={App1} />
+          <Route path="/app2" component={App2} />
+          <Route path="/loginClass" component={LoginClassComponent} />
+          <Route path="/loginFunctional" component={LoginFunctionalComponent} />
+        </div>
+      </Router>
+    );
+  }
 }
 
-const wrappedApp = connect()(RouterApp)
+const wrappedApp = connect()(RouterApp);
 
-export default wrappedApp
+export default wrappedApp;
