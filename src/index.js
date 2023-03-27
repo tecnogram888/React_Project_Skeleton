b2a7019c8a219e6c599d39a864762b57c5b1e585
tecnogram888 <civilizationarchitect@gmail.com>
2023-03-27 13:01:48 -0700

    React 18 Upgrade with VSCode Prettier Set Up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import _ from 'lodash';
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import RouterApp from './components/router'
+import _ from "lodash";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
+import RouterApp from "./components/router";
 
-
-render(
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <Provider store={store}>
     <RouterApp />
-  </Provider>,
-  document.getElementById('app')
-)
+  </Provider>
+);
