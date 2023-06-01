4db62e69454ba479639b283c50eafb6da1bb0d97
tecnogram888 <civilizationarchitect@gmail.com>
2023-05-31 20:25:09 -0700

    introduce tsx and Discovery Page

---
+++ src/components/DiscoveryPage/discoveryPageComponent.tsx
@@ -0,0 +1,9 @@
+import { FC, useState } from 'react';
+
+interface DiscoveryPageProps {
+  activities: Array<Activity>;
+}
+
+const DiscoveryPage: FC<DiscoveryPageProps> = ({ activities = [] }) => {
+  return <div></div>;
+};
